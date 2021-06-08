98293411448374012c1c6a1926f0dbb1dd5e183b
Alexei-Ruzheynikov <alexei_ruzheynikov@mail.ru>
2021-06-08 19:01:42 +0300

    created: last changes

--- js/main.js
+++ js/main.js
@@ -147,26 +147,45 @@ $(document).ready(function () {
     },
   });
 
+  var modalButton = $("[data-toggle=modal]");
+  var closemodalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closemodalButton.on("click", closeModal);
+
+  function openModal() {
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
+
   // Обработка форм
   $(".form").each(function () {
     $(this).validate({
       errorClass: "invalid",
       messages: {
         name: {
-          required: "Enter your name",
-          minlength: "At least 2 letters",
+          required: "Введите имя",
+          minlength: "Не менее 2 букв",
         },
         email: {
           required: "Введите email",
           email: "Формат: name@domain.com",
         },
         emailstwo: {
-          required: "Enter your contact email",
-          email: "Format email of name@domain.com",
+          required: "Введите email",
+          email: "Формат: name@domain.com",
         },
         phone: {
-          required: "Enter your phone number",
-          minlength: "At least 10 characters",
+          required: "Введите номер телефона",
+          minlength: "Не менее 10 символов",
         },
       },
     });
@@ -175,3 +194,10 @@ $(document).ready(function () {
   $(".phone").mask("+7 (999) 999-99-99");
 });
 //убрать комментарии
+$(document).keydown(function (e) {
+  var code = e.keyCode || e.which;
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  if (code == 27) modalDialog.removeClass("modal__dialog--visible");
+  if (code == 27) modalOverlay.removeClass("modal__overlay--visible");
+});
